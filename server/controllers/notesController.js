131380e893e3b3c531fb9029583d0d0ccf0e5108
gonids4545 <136231250+gonids4545@users.noreply.github.com>
2024-01-04 16:51:55 +0530

    Update notesController.js

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -2,9 +2,6 @@ const Note = require("../models/Note");
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 
-// @desc Get all notes
-// @route GET /notes
-// @access Private
 const getAllNotes = asyncHandler(async (req, res) => {
   // Get all notes from MongoDB
   const notes = await Note.find().lean();
@@ -13,10 +10,6 @@ const getAllNotes = asyncHandler(async (req, res) => {
   if (!notes?.length) {
     return res.status(400).json({ message: "No notes found" });
   }
-
-  // Add username to each note before sending the response
-  // See Promise.all with map() here: https://youtu.be/4lqJBBEpjRE
-  // You could also do this with a for...of loop
   const notesWithUser = await Promise.all(
     notes.map(async (note) => {
       const user = await User.findById(note.user).lean().exec();
@@ -27,9 +20,6 @@ const getAllNotes = asyncHandler(async (req, res) => {
   res.json(notesWithUser);
 });
 
-// @desc Create new note
-// @route POST /notes
-// @access Private
 const createNewNote = asyncHandler(async (req, res) => {
   const { user, title, text } = req.body;
 
@@ -59,9 +49,6 @@ const createNewNote = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc Update a note
-// @route PATCH /notes
-// @access Private
 const updateNote = asyncHandler(async (req, res) => {
   const { id, user, title, text, completed } = req.body;
 
@@ -98,9 +85,6 @@ const updateNote = asyncHandler(async (req, res) => {
   res.json(`'${updatedNote.title}' updated`);
 });
 
-// @desc Delete a note
-// @route DELETE /notes
-// @access Private
 const deleteNote = asyncHandler(async (req, res) => {
   const { id } = req.body;
 
